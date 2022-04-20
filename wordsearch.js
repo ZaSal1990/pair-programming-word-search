2b2614dba4e4beddef715c765a4931a55001110c
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-20 10:20:29 -0600

    fix buggy code, implemented vertical search using custom transpose fns created during LHL coursework

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//program to find word in 2D array vertically or horizontally
 
-module.exports = wordSearch
+const transpose = require('../../async/matrix-transposition.js');
+
+const wordSearch = (letters, word) => {
+  const horizontalFormation = letters.map(ls => ls.join(''));
+  for (let l of horizontalFormation) {
+    if (l.includes(word) === true) {
+      return true;
+    } else if (l.includes(word) === false) {
+      const verticalFormation = transpose(letters).map(ls => ls.join(''));
+      for (let l of verticalFormation) {
+        if (l.includes(word) === true) return true;
+      }
+    }
+  }
+  return false;
+};
+  
+// console.log(wordSearch([
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ], 'FRANK'));
+
+// console.log(wordSearch([
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ], 'SEINFELD'));
+
+module.exports = wordSearch;
